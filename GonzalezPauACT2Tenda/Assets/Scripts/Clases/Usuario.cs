7c595e651cb6bea41413a85fbc3a5fdb36c2101d
Paugp2000 <a24paugonpal@inspedralbes.cat>
2026-03-16 17:30:07 +0100

    Creacion de escena Login y Tienda

---
+++ GonzalezPauACT2Tenda/Assets/Scripts/Clases/Usuario.cs
@@ -0,0 +1,18 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using SQLite4Unity3d;
+using UnityEngine;
+
+public class Usuario
+{
+
+    [PrimaryKey, AutoIncrement] public int Id { get; set; }
+    public string NombreUsuario { get; set; }
+    public string Contraseña { get; set; }
+
+    public override string ToString()
+    {
+        return string.Format("[Usuario: Id={0}, NombreUsuario={1}, Contraseña={2})", Id, NombreUsuario, Contraseña);
+    }
+}
